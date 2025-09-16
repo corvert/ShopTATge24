251fc646242d40f789c083e3f9da6b5325e5c7e7
corvert <orvet.priimagi@gmail.com>
2025-09-16 11:41:11 +0300

    Add Kindergarten contoller and index view

---
+++ ShopTARge24/Shop.ApplicationServices/Services/KindergartenServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.Core.ServiceInterface;
+using Shop.Data;
+
+namespace Shop.ApplicationServices.Services
+{
+    public class KindergartenServices : IKindergartenServices
+    {
+        private readonly ShopContext _context;
+    }
+}
