23e75d0a992a19e9b9266ee75d3413d262591058
corvert <orvet.priimagi@gmail.com>
2025-10-13 10:51:14 +0300

    watherApi

---
+++ ShopTARge24/Shop.ApplicationServices/Services/WeatherForecastServices.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using Shop.Core.Dto;
+using Shop.Core.ServiceInterface;
+
+namespace Shop.ApplicationServices.Services
+{
+    public class WeatherForecastServices : IWeatherForecastServices
+    {
+        public async Task<AccuLocationWeatherResultDto> AccuWeatherResult(AccuLocationWeatherResultDto dto)
+        {
+            var response = "https://api.weatherapi.com/v1/current.json";
+
+            using (var client = new HttpClient())
+            {
+                var httpResponse = await client.GetAsync(response);
+                string json = await httpResponse.Content.ReadAsStringAsync();
+
+                //tallinn 127964
+                List<AccuLocationRootDto> weatherData = JsonSerializer.Deserialize<List<AccuLocationRootDto>>(json);
+
+
+            }
+            return dto;
+        }
+}
