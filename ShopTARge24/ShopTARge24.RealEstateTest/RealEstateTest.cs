c287c97218aef7025377626a4472c24afea5ecd2
corvert <orvet.priimagi@gmail.com>
2025-11-05 17:42:03 +0200

    contniue testing

--- ShopTARge24/ShopTARge24.RealEstateTest/RealEstateTest.cs
+++ ShopTARge24/ShopTARge24.RealEstateTest/RealEstateTest.cs
@@ -6,30 +6,102 @@ namespace ShopTARge24.RealEstateTest
 {
     public class RealEstateTest: TestBase
     {
+
+        private RealEstateDto realEstateDto = new()
+        {
+            //Id = Guid.NewGuid(),
+            Area = 120.5,
+            Location = "Test Location",
+            RoomNumber = 3,
+            BuildingType = "Apartment",
+            CreatedAt = DateTime.UtcNow,
+            ModifiedAt = DateTime.UtcNow
+
+
+        };
+
         [Fact]
-        public async Task Test1()
+        public async Task ShouldNot_AddEmptyRealEstate_WhenReturnResult()
         {
             //Arrange
-            RealEstateDto dto = new()
-            {
-                //Id = Guid.NewGuid(),
-                Area = 120.5,
-                Location = "Test Location",
-                RoomNumber = 3,
-                BuildingType = "Apartment",
-                CreatedAt = DateTime.UtcNow,
-                ModifiedAt = DateTime.UtcNow
-
-
-            };
+            realEstateDto = new RealEstateDto();
 
             //Act
-            var result = await Svc<IRealEstateServices>().Create(dto);
+            var result = await Svc<IRealEstateServices>().Create(realEstateDto);
 
 
             //Assert
             Assert.NotNull(result);
 
         }
+
+
+        //ShouldNot_GetByIdRealEstate_WhenReturnsNotEqual()
+        [Fact]
+        public async Task ShouldNot_GetByIdRealEstate_WhenReturnsNotEqual()
+        {
+            //Arrange
+            realEstateDto = new RealEstateDto();
+
+            //Act
+            var createdRealEstate = await Svc<IRealEstateServices>().Create(realEstateDto);
+            var differentId = Guid.NewGuid();
+            //var result = await Svc<IRealEstateServices>().DetailAsync(differentId);
+
+            //Assert
+            Assert.NotEqual(createdRealEstate.Id, differentId);
+           // Assert.Null(result);
+        }
+
+
+
+        //Should_GetByIdRealEstate_WhenReturnsEqual()
+        [Fact]
+        public async Task Should_GetByIdRealEstate_WhenReturnsEqual()
+        {
+            //Arrange
+            realEstateDto = new RealEstateDto();
+            //Act
+            var createdRealEstate = await Svc<IRealEstateServices>().Create(realEstateDto);
+            var result = await Svc<IRealEstateServices>().DetailAsync(createdRealEstate.Id.Value);
+            //Assert
+            Assert.Equal(createdRealEstate.Id, result.Id);
+        }
+
+
+
+        //Should_deleteByIdRealEstate_WhenDeleteRealEstate()
+        [Fact]
+        public async Task Should_deleteByIdRealEstate_WhenDeleteRealEstate()
+        {
+            //Arrange
+            realEstateDto = new RealEstateDto();
+            //Act
+            var createdRealEstate = await Svc<IRealEstateServices>().Create(realEstateDto);
+            var deletedRealEstate = await Svc<IRealEstateServices>().Delete(createdRealEstate.Id.Value);
+            var result = await Svc<IRealEstateServices>().DetailAsync(createdRealEstate.Id.Value);
+            //Assert
+            Assert.Equal(createdRealEstate.Id, deletedRealEstate.Id);
+            Assert.Null(result);
+        }
+
+
+
+        //ShouldNot_DeleteByIdRealEstate_WhenDidNotDeleteRealEstate()
+        [Fact]
+        public async Task ShouldNot_DeleteByIdRealEstate_WhenDidNotDeleteRealEstate()
+        {
+            //Arrange
+            realEstateDto = new RealEstateDto();
+            //Act
+            var createdRealEstate = await Svc<IRealEstateServices>().Create(realEstateDto);
+            var differentId = Guid.NewGuid();
+            var deletedRealEstate = await Svc<IRealEstateServices>().Delete(differentId);
+            var result = await Svc<IRealEstateServices>().DetailAsync(createdRealEstate.Id.Value);
+            //Assert
+            Assert.Null(deletedRealEstate);
+            Assert.NotNull(result);
+        }
+
     }
 }
--- ShopTARge24/ShopTARge24.RealEstateTest/TestBase.cs
+++ ShopTARge24/ShopTARge24.RealEstateTest/TestBase.cs
@@ -1,32 +1,41 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.ApplicationInsights.WorkerService;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
 using Shop.ApplicationServices.Services;
 using Shop.Core.ServiceInterface;
 using Shop.Data;
 using ShopTARge24.RealEstateTest.Macros;
+using ShopTARge24.RealEstateTest.Mock;
 
 namespace ShopTARge24.RealEstateTest
 {
     public abstract class TestBase
     {
         protected IServiceProvider serviceProvider { get; set; }
+       // protected IHostEnvironment hostEnvironment { get; set; }
         protected TestBase()
         {
             var services = new ServiceCollection();
             SetupServices(services);
             serviceProvider = services.BuildServiceProvider();
+           // hostEnvironment = services.BuildServiceProvider();
         }
 
         public virtual void SetupServices(IServiceCollection services)
         {
             services.AddScoped<IRealEstateServices, RealEstateServices>();
             services.AddScoped<IFileServices, FileServices>();
+            services.AddScoped<IHostEnvironment, MockIHostEnvironment>();
 
-            var mockHostEnvironment = new Mock<IHostEnvironment>();
-            mockHostEnvironment.Setup(m => m.ContentRootPath).Returns(Path.GetTempPath());
-            services.AddSingleton(mockHostEnvironment.Object);
+            // services.AddScoped<IHostEnvironment, HostEnvironment>();
+
+            //var mockHostEnvironment = new Moq.Mock<IHostEnvironment>();
+            //mockHostEnvironment.Setup(m => m.ContentRootPath).Returns(Path.GetTempPath());
+            //services.AddSingleton(mockHostEnvironment.Object);
 
             services.AddDbContext<ShopContext>(x =>
             {
