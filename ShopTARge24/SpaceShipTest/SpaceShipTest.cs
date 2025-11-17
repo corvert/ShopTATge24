722b822d23b834bb80a92853cdce6af93e50681a
corvert <orvet.priimagi@gmail.com>
2025-11-17 11:27:33 +0200

    Spaceship testing

---
+++ ShopTARge24/SpaceShipTest/SpaceShipTest.cs
@@ -0,0 +1,37 @@
+﻿using Shop.Core.Dto;
+using Shop.Core.ServiceInterface;
+
+namespace SpaceShipTest
+{
+    public class SpaceShipTest: TestBase
+    {
+        [Fact]
+        public async Task ShouldNot_CreateSpaceShip_PartialNullValues()
+        {
+            //arrange
+            SpaceShipDto dto = new SpaceShipDto
+            {
+                Name = "",
+                Classification = null,
+                BuildDate = DateTime.UtcNow,
+                Crew = null,
+                EnginePower = 5000,
+                CreatedAt = DateTime.UtcNow,
+                ModifiedAt = DateTime.UtcNow
+
+            };
+
+            //act
+            var result = await Svc<ISpaceshipServices>().Create(dto);
+
+            //assert
+            Assert.NotNull(result);
+            Assert.Null(result.Crew);
+            Assert.IsType<string>(result.Name);
+            Assert.IsType<int>(result.EnginePower);
+            Assert.Null(result.Classification);
+        }
+
+
+    }
+}
---
+++ ShopTARge24/SpaceShipTest/TestBase.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Shop.Data;
+using SpaceShipTest.Macros;
+using SpaceShipTest.Mock;
+using Shop.ApplicationServices.Services;
+using Shop.Core.ServiceInterface;
+
+namespace SpaceShipTest
+{
+    public abstract class TestBase
+    {
+        protected IServiceProvider serviceProvider { get; set; }
+        protected TestBase()
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+            serviceProvider = services.BuildServiceProvider();
+           
+        }
+
+        public virtual void SetupServices(IServiceCollection services)
+        {
+            services.AddScoped<ISpaceshipServices, SpaceshipServices>();
+            services.AddScoped<IFileServices, FileServices>();
+            services.AddScoped<IHostEnvironment, MockIHostEnvironment>();
+
+            services.AddDbContext<ShopContext>(x =>
+            {
+                x.UseInMemoryDatabase("TEST");
+                x.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            });
+
+            RegisterMacros(services);
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        protected T Svc<T>()
+        {
+            //Resolve service from the service provider
+            return serviceProvider.GetService<T>();
+        }
+
+        private void RegisterMacros(IServiceCollection services)
+        {
+            var macroBaseType = typeof(IMacros);
+
+            var macros = macroBaseType.Assembly.GetTypes()
+                .Where(t => macroBaseType.IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+
+            foreach (var macro in macros)
+            {
+                services.AddSingleton(macro);
+            }
+        }
+    }
+}
